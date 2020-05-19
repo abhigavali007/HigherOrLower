0b1bec92e309039cc5adf1f8fadfe1958d53f056
abhigavali007 <abhigavali007@gmail.com>
2020-05-19 23:20:41 +0530

    New Commit ErrorSolved

--- app/src/main/java/com/abhishek/higherorlower/MainActivity.java
+++ app/src/main/java/com/abhishek/higherorlower/MainActivity.java
@@ -10,16 +10,18 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    int rno;
     public void pressMe(View view)
     {
         EditText et= (EditText) findViewById(R.id.myTextField);
-        Random random= new Random();
-        int rno= random. nextInt(20)+1;
+
         int guess=Integer.parseInt(et.getText().toString());
+
         if(guess==rno)
         {
-            Toast.makeText(MainActivity.this, "Yes you picked it right", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Yes you picked it right!Try Again", Toast.LENGTH_SHORT).show();
+            Random random= new Random();
+            rno= random. nextInt(20)+1;
         }
         else if(guess>rno)
         {
@@ -36,5 +38,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Random random= new Random();
+        rno= random. nextInt(20)+1;
+
     }
 }
